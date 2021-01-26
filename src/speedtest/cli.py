9840539bcd768b0e3c3eb79798a481596c2fa4c8
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-26 22:32:53 +0100

    Implement read-log option in main method and strip units from saved data

--- src/speedtest/cli.py
+++ src/speedtest/cli.py
@@ -12,14 +12,24 @@ from . import utils, core
 from .__init__ import __version__, package_name
 
 
-@click.group(invoke_without_command=True)
-@click.version_option(version=__version__, prog_name=package_name)
+@click.group(invoke_without_command=True, help=style("Speedtest is a handy terminal application for assessing the performance of your network connectivity. It implements an alternative command line interface to Matt Martz' library.", fg='bright_magenta'))
+@click.version_option(version=__version__, prog_name=package_name, help=style("Read the log file", fg='yellow'))
+@click.option('--read-log', is_flag=True, default=False, help=style("Read the log file", fg='yellow'))
 @click.pass_context
-def cli(ctx):
+def cli(ctx, read_log):
     ctx.ensure_object(dict)
     ctx.obj['CONFIG'] = utils.read_configuration('speedtest.data', 'config.json')
     ctx.obj['PING'] = utils.read_configuration('speedtest.data', 'ping.json')
     ctx.obj['BANDWIDTH'] = utils.read_configuration('speedtest.data', 'bandwidth.json')
+
+    if read_log:
+        click.secho("\nLOG FILE CONTENT\n", fg='bright_magenta')
+        with open(utils.log_file_path(), mode='r', encoding='utf-8') as file_handler:
+            log = file_handler.readlines()
+            for line in log:
+                text = line.strip('\n').split(' - ')
+                click.secho(text[0], fg='yellow', nl=False)
+                click.echo(f" - {text[1]}")
 
 @cli.command(help=style("Configure default application settings.", fg='bright_green'))
 @click.option('--threads', type=click.INT, help=style("Set the number of speedtest threads.", fg='yellow'))
@@ -77,7 +87,9 @@ def ping(ctx, target, count, size, save):
 
     if save:
         tmp = ping.get('Results', [])
-        tmp.append({key: value.strip(' ') for key, value in results.items()})
+        blacklist = ['Date', 'Time', 'Ping Target', 'Country', 'ISP', 'IP']
+        sanitize = lambda key, value: value.strip(' ').strip('%msMB/s') if key not in blacklist else value.strip(' ')
+        tmp.append({key: sanitize(key, value) for key, value in results.items()})
         ping['Results'] = tmp
         utils.write_configuration('speedtest.data', 'ping.json', ping)
 
@@ -91,7 +103,6 @@ def internet(ctx, threads, upload, download, save):
     config: dict = ctx.obj['CONFIG']
     bandwidth: dict = ctx.obj['BANDWIDTH']
     
-    # click.echo("\033[A\033[A")
     click.secho("Network Connection Result", fg='bright_magenta')
     results = core.test_bandwidth(threads, upload, download)
     utils.print_dict('Name', 'Value', results)
