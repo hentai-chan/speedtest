3add790eac5190e467de6ab1ba6c896233df74ef
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-01-26 22:07:21 +0100

    Add log handler and print_on_warning to utils

--- src/speedtest/utils.py
+++ src/speedtest/utils.py
@@ -1,23 +1,45 @@
 #!/usr/bin/env python3
 
+import json
+import logging
 import os
 import platform
-import json
 from importlib.resources import path as resource_path
 from itertools import chain
+from pathlib import Path
 
 import click
 from colorama import Fore, Style
 
+from .__init__ import package_name
+
 #region i/o operations
+
+def log_file_path(target_dir=package_name) -> Path:
+    """
+    Make a `package_name` folder in the user's home directory, create a log
+    file (if there is none, else use the existsing one) and return its path.
+    """
+    directory = Path.home().joinpath(target_dir)
+    directory.mkdir(parents=True, exist_ok=True)
+    log_file = directory.joinpath(f"{target_dir}.log")
+    log_file.touch(exist_ok=True)
+    return log_file
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(asctime)s]::[%(levelname)s]::[%(name)s] - %(message)s')
+file_handler = logging.FileHandler(log_file_path())
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def read_configuration(resource: str, package: str) -> dict:
     """
     Return the content of `package` (a JSON file located in `resource`) as dictionary.
     """
     with resource_path(resource, package) as resource_handler:
-         with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
-             return json.load(file_handler)
+        with open(resource_handler, mode='r', encoding='utf-8') as file_handler:
+            return json.load(file_handler)
 
 def write_configuration(resource: str, package: str, params: dict) -> None:
     """
@@ -59,14 +81,21 @@ def print_on_success(message: str, verbose: bool=True) -> None:
     Print a success message if verbose is enabled.
     """
     if verbose:
-        click.secho(f"{Style.BRIGHT}{Fore.GREEN}{'[  OK  ]'.ljust(10, ' ')}{Style.RESET_ALL}{message}")
+        click.secho(f"{Style.BRIGHT}{Fore.GREEN}{'[  OK  ]'.ljust(12, ' ')}{Style.RESET_ALL}{message}")
+
+def print_on_warning(message: str, verbose: bool=True) -> None:
+    """
+    Print a formatted warning message if verbose is enabled.
+    """
+    if verbose:
+        click.secho(f"{Fore.YELLOW}{'[ WARNING ]'.ljust(12, ' ')}{Style.RESET_ALL}{message}")
 
 def print_on_error(message: str, verbose: bool=True) -> None:
     """
     Print an error message if verbose is enabled.
     """
     if verbose:
-        click.secho(f"{Style.BRIGHT}{Fore.RED}{'[ ERROR ]'.ljust(10, ' ')}{Style.RESET_ALL}{message}", err=True)
+        click.secho(f"{Style.BRIGHT}{Fore.RED}{'[ ERROR ]'.ljust(12, ' ')}{Style.RESET_ALL}{message}", err=True)
 
 def clear():
     """
