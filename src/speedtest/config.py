94e2410916028271783398e5de3746f5d247955e
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-10-03 23:44:29 +0200

    Add internal configuration file

---
+++ src/speedtest/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+LOGFILE = 'error.log'
+CONFIGFILE = 'config.json'
+PINGFILE = 'ping.csv'
+BANDWIDTHFILE = 'bandwidth.csv'
+
+#region colors and styles
+
+DIM = '\033[2m'
+NORMAL = ''
+BRIGHT = '\033[1m'
+RESET_ALL = '\033[0m'
+
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+
+#endregion
